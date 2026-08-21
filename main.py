36705c7fddfa37690b0e6e9a1ee3c2e37067f225
Kiran-Samuel <kirankoshy41@gmail.com>
2026-08-21 16:35:52 +0400

    Data Validation added

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 # tells your pipeline which stages to run and in what order.
 from Supplier_clustering.utils.logger import logger
 from Supplier_clustering.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from Supplier_clustering.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Validation stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataValidationTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
